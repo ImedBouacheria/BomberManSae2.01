dd8496283a5f6f2451c5b14763b965bd156db567
ImedBouacheria <imed.bouacheria-kebir@etu.univ-amu.fr>
2025-06-05 09:54:25 +0200

    Add files via upload

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.bombermansae201 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.bombermansae201 to javafx.fxml;
+    exports com.example.bombermansae201;
+}
